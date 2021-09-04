02f932eec0f1c8b663224d4c1bd0109516ed46ce
Artem Sergieiev <66088984+ArtemS83@users.noreply.github.com>
2021-09-04 14:40:53 +0200

    Updates

--- static/js/main.8294ee13.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpCodMi-test"]=this["webpackJsonpCodMi-test"]||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var c=n(1),i=n.n(c),r=n(3),s=n.n(r),o=(n(8),n(9),n(0));var d=function(){return Object(o.jsx)("div",{children:Object(o.jsx)("h1",{children:"Hello React"})})};s.a.render(Object(o.jsx)(i.a.StrictMode,{children:Object(o.jsx)(d,{})}),document.getElementById("root"))},9:function(t,e,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.8294ee13.chunk.js.map
